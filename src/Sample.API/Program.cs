6db906a3db0efd5b75fe67e3724b30aad3f9ebe1
kursad.koc <kursad.koc@tarimorman.gov.tr>
2024-11-12 18:01:05 +0300

    init

---
+++ src/Sample.API/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#if(EnableSwagger)
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+#endif
+
+var app = builder.Build();
+
+#if(EnableSwagger)
+    app.UseSwagger();
+    app.UseSwaggerUI();
+#endif
+
+app.MapGet("/", () => "it works");
+
+app.Run();
