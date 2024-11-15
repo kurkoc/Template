4cc39321368dd3ccfe844b35786fa4a0cfc4208e
kursad.koc <kursad.koc@tarimorman.gov.tr>
2024-11-15 16:21:43 +0300

    jwt interface güncellendi

--- src/TemplateSolution.Infrastructure/Authentication/Abstractions/IJwtService.cs
+++ src/TemplateSolution.Infrastructure/Authentication/Abstractions/IJwtService.cs
@@ -2,5 +2,5 @@ namespace TemplateSolution.Infrastructure.Authentication.Abstractions;
 
 public interface IJwtService
 {
-    
+    Task<dynamic> GenerateToken(Guid userId, string firstname, string lastname, string email);
 }
